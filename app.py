e14f5bfe7c99850b9db2c9a4d98db1c9814592e1
sh-andriy <sheredko.andriy@gmail.com>
2023-03-01 22:16:15 +0200

    v0.0.5 - created page templates using Jinja2

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
