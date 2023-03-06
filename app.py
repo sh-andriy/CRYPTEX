6cf7a4d69d358b12ccb01a4b45d8f69198cd6a61
sh-andriy <sheredko.andriy@gmail.com>
2023-03-06 15:27:20 +0200

    v0.0.8 - fixed some issues

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from os import environ
-from datetime import datetime
+# from datetime import datetime
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-DB_USERNAME = environ.get('DB_USERNAME', 'user')
+DB_USERNAME = environ.get('DB_USERNAME', 'username')
 DB_PASSWORD = environ.get('DB_PASSWORD', 'password')
 
 # adding database
@@ -17,22 +17,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{DB_USERNAME}:{DB_PASSWORD}@lo
 db = SQLAlchemy(app)
 
 
-
-
 @app.route('/')
 def home():
+    """
+    function that renders home.html
+    """
     return render_template('home.html')
 
 
 @app.route('/register')
 def register():
+    """
+    function that renders register.html
+    """
     return render_template('register.html')
 
 
 @app.route('/login')
 def login():
     """
-    function render login.html
+    function that renders login.html
     """
     return render_template('login.html')
 
